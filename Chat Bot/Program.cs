79f691f60b7426e52a97096dbcfa7220ea670690
cpaul0013 <90057953+cpaul0013@users.noreply.github.com>
2021-09-13 12:25:18 -0400

    Add files via upload

---
+++ Chat Bot/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Chat_Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What would you like to say to the bot?: hello, bye, sup, hello there ");
+            string response = Console.ReadLine();
+            response.ToLower();
+            bool keepTalking = true;
+            while (keepTalking == true)
+            {
+                if (response == "hello")
+                {
+                    Console.WriteLine("Hi good to see you");
+
+                    bool helloResponse = false;
+                    while (helloResponse == false)
+                    {
+                        Console.Write("What would you like to say to the bot?: hello, bye, sup, hello there ");
+                        string response2 = Console.ReadLine();
+                        response = response2;
+                        if (response == "hello")
+                        {
+                            Console.WriteLine("I am sorry you have already said that.");
+                        }
+                        else { helloResponse = true; }
+                    }
+
+                }
+                else if (response == "sup")
+                {
+                    Console.WriteLine("I am good.");
+                    bool supResponse = false;
+                    while (supResponse == false)
+                    {
+                        Console.Write("What would you like to say to the bot?: hello, bye, sup, hello there ");
+                        string response2 = Console.ReadLine();
+                        response = response2;
+                        if (response == "sup")
+                        {
+                            Console.WriteLine("I am sorry you have already said that.");
+                        }
+                        else { supResponse = true; }
+                    }
+                }
+                else if (response == "hello there")
+                {
+                    Console.WriteLine("General Kenobi");
+                    bool helloThereResponse = false;
+                    while (helloThereResponse == false)
+                    {
+                        Console.Write("What would you like to say to the bot?: hello, bye, sup, hello there ");
+                        string response2 = Console.ReadLine();
+                        response = response2;
+                        if (response == "hello there")
+                        {
+                            Console.WriteLine("I am sorry you have already said that.");
+                        }
+                        else { helloThereResponse = true; }
+
+                    }
+                }
+                else { Console.WriteLine("Good bye!");
+                    keepTalking = false;
+                }
+            }
+        }
+    }
+}
